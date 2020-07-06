d190bf3d7c2b7412acb449fe2445da50e4f37d8c
Dewan <dtahmid000@citymail.cuny.edu>
2020-07-06 17:59:03 -0400

    Formatted and Commented Nodes's header file

--- src/node2.h
+++ src/node2.h
@@ -125,47 +125,53 @@
 #define ASSIGNMENT_NODE1_H
 #include <cstdlib> // Provides size_t and NULL
 
-namespace assignment_sequence2
-{
-    class node
-    {
+namespace assignment_sequence2 {
+    class node {
     public:
-	// TYPEDEF
-	typedef double value_type;
+    	// TYPEDEF
+    	typedef double value_type;
 
-	// CONSTRUCTOR
-	node(const value_type& init_data = value_type( ), node* init_link = NULL)
-	{ data_field = init_data; link_field = init_link; }
+    	// CONSTRUCTOR
+    	node(const value_type& init_data = value_type(), node* init_link = NULL) { 
+            data_field = init_data, link_field = init_link; 
+        }
 
-	// Member functions to set the data and link fields:
-    void set_data(const value_type& new_data) { data_field = new_data; }
-    void set_link(node* new_link)             { link_field = new_link; }
+    	// Member functions to set the data and link fields:
+        void set_data(const value_type& new_data) {data_field = new_data;}
+        void set_link(node* new_link) {link_field = new_link;}
 
-	// Constant member function to retrieve the current data:
-	value_type data( ) const { return data_field; }
+    	// Constant member function to retrieve the current data:
+    	value_type data() const {return data_field;}
 
-	// Two slightly different member functions to retreive
-	// the current link:
-	const node* link( ) const { return link_field; }
-    node* link( )             { return link_field; }
+    	// Two slightly different member functions to retreive the current link:
+    	const node* link() const {return link_field;}
+        node* link() {return link_field;}
 
     private:
-	value_type data_field;
-	node* link_field;
+    	value_type data_field; /* Contains data of node */
+    	node* link_field; /* Pointer to the next node */
     };
 
     // FUNCTIONS for the linked list toolkit
-    std::size_t list_length(const node* head_ptr);
-    void list_head_insert(node*& head_ptr, const node::value_type& entry);
-    void list_insert(node* previous_ptr, const node::value_type& entry);
-    node* list_search(node* head_ptr, const node::value_type& target);
+    std::size_t list_length(const node* head_ptr); /* O(n): Calculates how many nodes there are */
+
+    void list_head_insert(node*& head_ptr, const node::value_type& entry); /* O(1): Inserts node at head */
+    void list_insert(node* previous_ptr, const node::value_type& entry); /* O(1): Inserts node after another node */
+
+    void list_head_remove(node*& head_ptr); /* O(1): Removes node at head */
+    void list_remove(node* previous_ptr); /* O(1): Removes node after another node */
+
+    /* O(n): Serial Search for a node with specified value */
+    node* list_search(node* head_ptr, const node::value_type& target); 
     const node* list_search(const node* head_ptr, const node::value_type& target);
+
+    /* O(n): Finds a node at specified location */
+    /* Essentially array[i] but for the linked list */
     node* list_locate(node* head_ptr, std::size_t position);
     const node* list_locate(const node* head_ptr, std::size_t position);
-    void list_head_remove(node*& head_ptr);
-    void list_remove(node* previous_ptr);
-    void list_clear(node*& head_ptr);
-    void list_copy(const node* source_ptr, node*& head_ptr, node*& tail_ptr);
+    
+    void list_clear(node*& head_ptr); /* O(n): Clears the linked list */
+    void list_copy(const node* source_ptr, node*& head_ptr, node*& tail_ptr); /* O(n): Copies elements of one linked list to another */
 }
 
 #endif
