b8ea12b22cd35cbc5ed4a0cf2ed213d368522a96
Dewan <dtahmid000@citymail.cuny.edu>
2020-07-07 21:48:33 -0400

    Improved insert and attach to change current_ptr location

--- src/sequence2.cpp
+++ src/sequence2.cpp
@@ -14,18 +14,22 @@ namespace assignment_sequence2 {
 		current_ptr = NULL;
 		many_nodes = 0;
 	}
+
 	sequence::sequence(const sequence& source) {
 		list_copy(source.head_ptr, head_ptr, tail_ptr);
 		many_nodes = source.many_nodes;
 		current_ptr = source.current_ptr;
 	}
+
 	sequence::~sequence() {
 		list_clear(head_ptr);
 	}
+
 	// MODIFICARION MEMBER FUNCTIONS
 	void sequence::start() {
 		current_ptr = head_ptr;
 	}
+
 	void sequence::advance() {
 		current_ptr = current_ptr -> link();
 	}
@@ -33,33 +37,51 @@ namespace assignment_sequence2 {
 	void sequence::insert(const value_type& entry) {
 		if (many_nodes == 0) {
 			list_head_insert(head_ptr);
+			current_ptr = NULL;
 		} 
 		else if (is_item() == false) {
 			list_insert(tail_ptr);
+			current_ptr = head_ptr;
 		} 
 		else {
-			list_insert(cursor);
+			list_insert(current_ptr);
+			current_ptr = current_ptr -> link();
 		}
 		++many_nodes;
 	}
+
 	void sequence::attach(const value_type& entry) {
 		if (many_nodes == 0) {
 			list_head_insert(head_ptr);
+			current_ptr = head_ptr;
 		} 
 		else if (is_item() == false) {
 			list_head_insert(head_ptr);
+			current_ptr = tail_ptr;
 		}
 		else {
-			list_insert(cursor);
+			list_insert(current_ptr);
+			current_ptr = current_ptr -> link();
 		}
 		++many_nodes;
 	}
+
 	void sequence::remove_current() {
 		assert(is_item());
-		if (current_ptr == head_ptr) {
+		if (many_nodes == 1) {
 			list_head_remove(head_ptr);
-		} else {
+			current_ptr = NULL;
+		}
+		else if (current_ptr == head_ptr) {
+			list_head_remove(head_ptr);
+			current_ptr = head_ptr;
+		} else if (current_ptr == tail_ptr) {
+			list_remove(tail_ptr);
+			current_ptr = tail_ptr;
+		}
+		else {
 			list_remove(current_ptr);
+			current_ptr = current_ptr -> link();
 		}
 		--many_nodes;
 	}
